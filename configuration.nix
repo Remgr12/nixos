0d9f55e5df10e13de2b49718a59f6c952249fc27
Zsombor Simon <zsombor@remgr.dev>
2026-05-17 13:25:20 +0200

    changed from lazyvim to nixvim

--- configuration.nix
+++ configuration.nix
@@ -186,15 +186,56 @@ in
       };
 
       programs.kitty = {
-        enable = true;
-        settings = {
-          hide_window_decorations = "yes";
-          window_padding_width = 4;
-          shell = "zsh";
-          confirm_os_window_close = 0;
+          enable = true;
+          
+          settings = {
+            # Core Settings
+            hide_window_decorations = "yes";
+            window_padding_width = 4;
+            shell = "zsh";
+            confirm_os_window_close = 0;
+      
+            # Theme: Nord
+            foreground = "#D8DEE9";
+            background = "#2E3440";
+            selection_foreground = "#000000";
+            selection_background = "#FFFACD";
+            url_color = "#0087BD";
+            cursor = "#81A1C1";
+      
+            # black
+            color0 = "#3B4252";
+            color8 = "#4C566A";
+      
+            # red
+            color1 = "#BF616A";
+            color9 = "#BF616A";
+      
+            # green
+            color2 = "#A3BE8C";
+            color10 = "#A3BE8C";
+      
+            # yellow
+            color3 = "#EBCB8B";
+            color11 = "#EBCB8B";
+      
+            # blue
+            color4 = "#81A1C1";
+            color12 = "#81A1C1";
+      
+            # magenta
+            color5 = "#B48EAD";
+            color13 = "#B48EAD";
+      
+            # cyan
+            color6 = "#88C0D0";
+            color14 = "#8FBCBB";
+      
+            # white
+            color7 = "#E5E9F0";
+            color15 = "#ECEFF4";
+          };
         };
-        extraConfig = "include ./nord.conf";
-      };
 
       programs.spicetify = 
         let
--- flake.nix
+++ flake.nix
@@ -23,6 +23,11 @@
       url = "github:JakeStanger/ironbar";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
